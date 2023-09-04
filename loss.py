ab15c8216ed9119e8fdaf6cbb6d9681a4f88859c
zahrasa <36826126+zahrasa@users.noreply.github.com>
2023-09-05 01:32:03 +0330

    Update loss.py
    add dice metric

--- loss.py
+++ loss.py
@@ -1,6 +1,7 @@
 import tensorflow.keras.backend as K
 from tensorflow.keras.losses import categorical_crossentropy
-
+import keras
+import keras.backend as K
 
 def generalized_dice(y_true, y_pred):
     
@@ -33,7 +34,24 @@ def custom_loss(y_true, y_pred):
     
     return generalized_dice_loss(y_true, y_pred) + 1.25 * categorical_crossentropy(y_true, y_pred)
     
-    
+# add to loss.py
+  # dice loss as defined above for 4 classes
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 4
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
     
     
     
